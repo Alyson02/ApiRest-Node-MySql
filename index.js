425e8a016d2afd72490ae0e66a5bfdd4f01373c1
Alyson <alyson.subrim2002@gmail.com>
2022-03-16 09:20:57 -0300

    Implementa banco de dados
    Implementação de banco de dados mysql com o blib mysql

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
 
-const app = customExpress();
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro);
+    }else{
+        console.log("Conectado com sucesso!");
 
-app.listen(3000, () => console.log('Servidor rodadndo  na porta 3000'));
+        const app = customExpress();
+        app.listen(3000, () => console.log('Servidor rodadndo  na porta 3000'));
+    }
+})
+
+
 
 
 
