19b2a074552fd1de3707bbce1a6e5472fd90eac1
Alyson <alyson.subrim2002@gmail.com>
2022-03-15 23:11:27 -0300

    Requisições POST
    Utilizando a blib body-parser para ouvir requisições post
    enviadas pelo corpo da pagina

--- Controllers/Atendimento.js
+++ Controllers/Atendimento.js
@@ -1,3 +1,9 @@
 module.exports = app => {
+
     app.get('/atendimentos', (req, res) => res.send("Servidor rodando, voce sera atendido, ok!"));
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+        res.send("Você realizou um post");
+    });
 }
--- config/customExpress.js
+++ config/customExpress.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = () => {
 
     const app = express();
+
+    //Usando o metodo use do exprees para exepcificar requisições que usam o body
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
 
     consign()
         .include('controllers')
