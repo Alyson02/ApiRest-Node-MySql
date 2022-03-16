8c423314e43386ef849cd4cdf56edb12e0776c91
Alyson <alyson.subrim2002@gmail.com>
2022-03-15 22:48:11 -0300

    Separa responsabilidade config
    separação da responsabilidade de configurar o servidor da index
    para pasta config no arquivo customExpress

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const consign = require('consign');
+const customExpress = require('./config/customExpress');
 
-const app = express();
-
-consign()
-    .include('controllers')
-    .into(app)
+const app = customExpress();
 
 app.listen(3000, () => console.log('Servidor rodadndo  na porta 3000'));
 
