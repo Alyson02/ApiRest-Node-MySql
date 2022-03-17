6995d4e1f2f6450046b44c1c905e1e8db0f46633
Alyson <alyson.subrim2002@gmail.com>
2022-03-16 23:11:17 -0300

    Implementa validações personalizadas
    Implementação de respostas personalizadas por meio de um objeto de erro

--- models/Atendimento.js
+++ models/Atendimento.js
@@ -2,20 +2,44 @@ const conexao = require('../infraestrutura/conexao')
 const moment = require('moment');
 
 class Atendimento{
-    adiciona(atendimento){
+    adiciona(atendimento, res){
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
 
-        const sql = "INSERT INTO ATENDIMENTOS SET ?";
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.Cliente.length >= 5;
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) =>
-        {
-            if(erro)
-                console.log(erro);
-            else
-                console.log(resultados);
-        })
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if(existemErros)
+            res.status(400).json(erros);
+        else{
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+
+            const sql = "INSERT INTO ATENDIMENTOS SET ?";
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) =>
+            {
+                if(erro)
+                    res.status(400).json(erro);
+                else
+                    res.status(201).json(resultados);
+            })
+        }
     }
 }
 
